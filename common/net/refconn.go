fc16a801a7f9da64e5f83deaf8bc01d6a2414f10
LittleRey <1017537456@qq.com>
2023-04-29 19:08:22 +0800

    1

---
+++ common/net/refconn.go
@@ -0,0 +1,126 @@
+package net
+
+import (
+	"net"
+	"runtime"
+	"time"
+
+	"github.com/Dreamacro/clash/common/buf"
+)
+
+type refConn struct {
+	conn ExtendedConn
+	ref  any
+}
+
+func (c *refConn) Read(b []byte) (n int, err error) {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.Read(b)
+}
+
+func (c *refConn) Write(b []byte) (n int, err error) {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.Write(b)
+}
+
+func (c *refConn) Close() error {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.Close()
+}
+
+func (c *refConn) LocalAddr() net.Addr {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.LocalAddr()
+}
+
+func (c *refConn) RemoteAddr() net.Addr {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.RemoteAddr()
+}
+
+func (c *refConn) SetDeadline(t time.Time) error {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.SetDeadline(t)
+}
+
+func (c *refConn) SetReadDeadline(t time.Time) error {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.SetReadDeadline(t)
+}
+
+func (c *refConn) SetWriteDeadline(t time.Time) error {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.SetWriteDeadline(t)
+}
+
+func (c *refConn) Upstream() any {
+	return c.conn
+}
+
+func (c *refConn) ReadBuffer(buffer *buf.Buffer) error {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.ReadBuffer(buffer)
+}
+
+func (c *refConn) WriteBuffer(buffer *buf.Buffer) error {
+	defer runtime.KeepAlive(c.ref)
+	return c.conn.WriteBuffer(buffer)
+}
+
+func (c *refConn) ReaderReplaceable() bool { // Relay() will handle reference
+	return true
+}
+
+func (c *refConn) WriterReplaceable() bool { // Relay() will handle reference
+	return true
+}
+
+var _ ExtendedConn = (*refConn)(nil)
+
+func NewRefConn(conn net.Conn, ref any) net.Conn {
+	return &refConn{conn: NewExtendedConn(conn), ref: ref}
+}
+
+type refPacketConn struct {
+	pc  net.PacketConn
+	ref any
+}
+
+func (pc *refPacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.ReadFrom(p)
+}
+
+func (pc *refPacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.WriteTo(p, addr)
+}
+
+func (pc *refPacketConn) Close() error {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.Close()
+}
+
+func (pc *refPacketConn) LocalAddr() net.Addr {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.LocalAddr()
+}
+
+func (pc *refPacketConn) SetDeadline(t time.Time) error {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.SetDeadline(t)
+}
+
+func (pc *refPacketConn) SetReadDeadline(t time.Time) error {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.SetReadDeadline(t)
+}
+
+func (pc *refPacketConn) SetWriteDeadline(t time.Time) error {
+	defer runtime.KeepAlive(pc.ref)
+	return pc.pc.SetWriteDeadline(t)
+}
+
+func NewRefPacketConn(pc net.PacketConn, ref any) net.PacketConn {
+	return &refPacketConn{pc: pc, ref: ref}
+}
---
+++ tunnel/connection.go
@@ -0,0 +1,79 @@
+package tunnel
+
+import (
+	"errors"
+	"net"
+	"net/netip"
+	"time"
+
+	N "github.com/Dreamacro/clash/common/net"
+	"github.com/Dreamacro/clash/common/pool"
+	C "github.com/Dreamacro/clash/constant"
+	"github.com/Dreamacro/clash/log"
+)
+
+func handleUDPToRemote(packet C.UDPPacket, pc C.PacketConn, metadata *C.Metadata) error {
+	addr := metadata.UDPAddr()
+	if addr == nil {
+		return errors.New("udp addr invalid")
+	}
+
+	if _, err := pc.WriteTo(packet.Data(), addr); err != nil {
+		return err
+	}
+	// reset timeout
+	_ = pc.SetReadDeadline(time.Now().Add(udpTimeout))
+
+	return nil
+}
+
+func handleUDPToLocal(packet C.UDPPacket, pc net.PacketConn, key string, oAddr, fAddr netip.Addr) {
+	buf := pool.Get(pool.UDPBufferSize)
+	defer func() {
+		_ = pc.Close()
+		closeAllLocalCoon(key)
+		natTable.Delete(key)
+		_ = pool.Put(buf)
+	}()
+
+	for {
+		_ = pc.SetReadDeadline(time.Now().Add(udpTimeout))
+		n, from, err := pc.ReadFrom(buf)
+		if err != nil {
+			return
+		}
+
+		fromUDPAddr := from.(*net.UDPAddr)
+		_fromUDPAddr := *fromUDPAddr
+		fromUDPAddr = &_fromUDPAddr // make a copy
+		if fromAddr, ok := netip.AddrFromSlice(fromUDPAddr.IP); ok {
+			if fAddr.IsValid() && (oAddr.Unmap() == fromAddr.Unmap()) {
+				fromUDPAddr.IP = fAddr.Unmap().AsSlice()
+			} else {
+				fromUDPAddr.IP = fromAddr.Unmap().AsSlice()
+			}
+		}
+
+		_, err = packet.WriteBack(buf[:n], fromUDPAddr)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func closeAllLocalCoon(lAddr string) {
+	natTable.RangeLocalConn(lAddr, func(key, value any) bool {
+		conn, ok := value.(*net.UDPConn)
+		if !ok || conn == nil {
+			log.Debugln("Value %#v unknown value when closing TProxy local conn...", conn)
+			return true
+		}
+		conn.Close()
+		log.Debugln("Closing TProxy local conn... lAddr=%s rAddr=%s", lAddr, key)
+		return true
+	})
+}
+
+func handleSocket(ctx C.ConnContext, outbound net.Conn) {
+	N.Relay(ctx.Conn(), outbound)
+}
